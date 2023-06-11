0f1a91d5aa012bb6e59b7fdecc5f0e474eabb918
Rafay Khan <abdulrafaykhan857@gmail.com>
2023-06-11 22:01:59 +0500

    payment

--- server.js
+++ server.js
@@ -1,103 +1,195 @@
-  import express from "express";
-  import axios from "axios";
-  import bodyParser from "body-parser";
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
 
-  const app = express();
-  const port = 4000;
+const app = express();
+const port = 4000;
 
-  // server is live on url: https://panel.leigonsoft.site/
+// SSL credentials
+const ssl_merchant_id = "0023024";
+const ssl_user_id = "apiuser";
+const ssl_pin =
+  "KABX047M4VXOEV2U3JS7X7J5BZB6LQZSKFFFORJWM1JDAP1W6WWN9WOVAM3LV7FP";
 
-  app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://leigonsoft.site");
-    res.setHeader("Access-Control-Allow-Methods", "POST");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    next();
-  });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
-  app.options("/create-payment-session", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://leigonsoft.site");
-    res.setHeader("Access-Control-Allow-Methods", "POST");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.sendStatus(200);
-  });
+app.options("/create-payment-session", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
+});
 
-  app.post("/create-payment-session", async (req, res) => {
-    try {
-      const clientIP = req.ip;
+app.post("/create-payment-session", async (req, res) => {
+  try {
+    const orderData = {
+      total: {
+        amount: req.body.amount,
+        currencyCode: "EUR",
+      },
+      description: "Product purchase",
+    };
 
-      console.log("Client IP:", clientIP);
-
-      const orderData = {
-        total: {
-          amount: req.body.amount,
-          currencyCode: "EUR",
+    const orderResponse = await axios.post(
+      "https://api.eu.convergepay.com/orders",
+      orderData,
+      {
+        headers: {
+          Authorization: `Bearer ${ssl_pin}`,
+          merchant_id: ssl_merchant_id,
+          user_id: ssl_user_id,
         },
-        description: "parts",
-      };
+      }
+    );
 
-      const ssl_merchant_id = "0023024";
-      const ssl_user_id = "apiuser";
-      const ssl_pin =
-        "KABX047M4VXOEV2U3JS7X7J5BZB6LQZSKFFFORJWM1JDAP1W6WWN9WOVAM3LV7FP";
+    const orderUrl = orderResponse.data.href;
 
-      const orderResponse = await axios.post(
-        "https://api.eu.convergepay.com/orders",
-        orderData,
-        {
-          headers: {
-            Authorization: `Bearer ${ssl_pin}`,
-            merchant_id: ssl_merchant_id,
-            user_id: ssl_user_id,
-          },
-        }
-      );
-      const orderUrl = orderResponse.data.href;
+    const paymentSessionData = {
+      order: orderUrl,
+      returnUrl: "https://example.com/return",
+      cancelUrl: "https://example.com/cancel",
+      doCreateTransaction: true,
+    };
 
-      const paymentSessionData = {
-        order: orderUrl,
-        returnUrl: "https://leigonsoft.site/return",
-        cancelUrl: "https://leigonsoft.site/cancel",
-        doCreateTransaction: true,
-      };
+    const paymentSessionResponse = await axios.post(
+      "https://api.eu.convergepay.com/payment-sessions",
+      paymentSessionData,
+      {
+        headers: {
+          Authorization: `Bearer ${ssl_pin}`,
+          merchant_id: ssl_merchant_id,
+          user_id: ssl_user_id,
+        },
+      }
+    );
 
-      const paymentSessionResponse = await axios.post(
-        "https://api.eu.convergepay.com/payment-sessions",
-        paymentSessionData,
-        {
-          headers: {
-            Authorization: `Bearer ${ssl_pin}`,
-            merchant_id: ssl_merchant_id,
-            user_id: ssl_user_id,
-          },
-        }
-      );
-      const paymentSessionUrl = paymentSessionResponse.data.href;
+    const paymentSessionUrl = paymentSessionResponse.data.href;
 
-      const paymentSessionUrls = {
-        lightboxUrl: paymentSessionUrl,
-        returnUrl: "https://leigonsoft.site/return",
-        cancelUrl: "https://leigonsoft.site/cancel",
-      };
+    res.setHeader("Access-Control-Allow-Origin", "https://leigonsoft.site");
+    res.setHeader("Access-Control-Allow-Methods", "POST");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
 
-      res.setHeader(
-        "Access-Control-Allow-Origin",
-        "https://leigonsoft.site"
-      );
-      res.setHeader("Access-Control-Allow-Methods", "POST");
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-      res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.json({ paymentUrl: paymentSessionUrl });
+  } catch (error) {
+    console.error("Error creating payment session:", error);
+    res.status(500).json({ error: "Failed to create payment session" });
+  }
+});
 
-      res.json(paymentSessionUrls);
-    } catch (error) {
-      console.error("Error creating payment session:", error);
-      res.status(500).json({ error: "Failed to create payment session" });
-    }
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+// import express from "express";
+// import axios from "axios";
+// import bodyParser from "body-parser";
+
+// const app = express();
+// const port = 4000;
+
+// // server is live on url: https://panel.leigonsoft.site/
+
+// app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://leigonsoft.site");
+//   res.setHeader("Access-Control-Allow-Methods", "POST");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
+
+// app.options("/create-payment-session", (req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://leigonsoft.site");
+//   res.setHeader("Access-Control-Allow-Methods", "POST");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.sendStatus(200);
+// });
+
+// app.post("/create-payment-session", async (req, res) => {
+//   try {
+//     const clientIP = req.ip;
+
+//     console.log("Client IP:", clientIP);
+
+//     const orderData = {
+//       total: {
+//         amount: req.body.amount,
+//         currencyCode: "EUR",
+//       },
+//       description: "parts",
+//     };
+
+//     const ssl_merchant_id = "0023024";
+//     const ssl_user_id = "apiuser";
+//     const ssl_pin =
+//       "KABX047M4VXOEV2U3JS7X7J5BZB6LQZSKFFFORJWM1JDAP1W6WWN9WOVAM3LV7FP";
+
+//     const orderResponse = await axios.post(
+//       "https://api.eu.convergepay.com/orders",
+//       orderData,
+//       {
+//         headers: {
+//           Authorization: `Bearer ${ssl_pin}`,
+//           merchant_id: ssl_merchant_id,
+//           user_id: ssl_user_id,
+//         },
+//       }
+//     );
+//     const orderUrl = orderResponse.data.href;
+
+//     const paymentSessionData = {
+//       order: orderUrl,
+//       returnUrl: "https://leigonsoft.site/return",
+//       cancelUrl: "https://leigonsoft.site/cancel",
+//       doCreateTransaction: true,
+//     };
+
+//     const paymentSessionResponse = await axios.post(
+//       "https://api.eu.convergepay.com/payment-sessions",
+//       paymentSessionData,
+//       {
+//         headers: {
+//           Authorization: `Bearer ${ssl_pin}`,
+//           merchant_id: ssl_merchant_id,
+//           user_id: ssl_user_id,
+//         },
+//       }
+//     );
+//     const paymentSessionUrl = paymentSessionResponse.data.href;
+
+//     const paymentSessionUrls = {
+//       lightboxUrl: paymentSessionUrl,
+//       returnUrl: "https://leigonsoft.site/return",
+//       cancelUrl: "https://leigonsoft.site/cancel",
+//     };
+
+//     res.setHeader(
+//       "Access-Control-Allow-Origin",
+//       "https://leigonsoft.site"
+//     );
+//     res.setHeader("Access-Control-Allow-Methods", "POST");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+
+//     res.json(paymentSessionUrls);
+//   } catch (error) {
+//     console.error("Error creating payment session:", error);
+//     res.status(500).json({ error: "Failed to create payment session" });
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
